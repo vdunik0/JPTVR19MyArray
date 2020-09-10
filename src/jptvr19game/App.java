932ea2c6daa35c65a26358ece31089ecb020f154
pupil <pupil@ivkhk.ee>
2020-09-10 09:32:25 +0300

    first commit

---
+++ src/jptvr19game/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr19game;
+
+/**
+ *
+ * @author pupil
+ */
+public class App {
+    public void run(){
+        System.out.println("Hello");
+    }
+}
